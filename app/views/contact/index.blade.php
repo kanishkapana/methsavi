ea21b3f49f4f4ad8b178cad8ef28736fb95cb149
kanishka <kanishka@lalamove.com>
2016-12-22 01:11:40 +0800

    UPT: Fixed typo

--- app/views/contact/index.blade.php
+++ app/views/contact/index.blade.php
@@ -7,7 +7,7 @@
         <div class='padding-top-40'>
             <span class=''>Email : </span>kanishkapanamaldeniya@gmail.com
             </br></br>
-            <span class=''>Facecook Page : </span><a target="_blank" href='https://www.facebook.com/poweroflovingkindness/'>power of loving kindness</a>
+            <span class=''>FaceBook Page : </span><a target="_blank" href='https://www.facebook.com/poweroflovingkindness/'>power of loving kindness</a>
             </br></br>
             <span class=''>Mobile Number : </span>+852-55882545
         </div>
